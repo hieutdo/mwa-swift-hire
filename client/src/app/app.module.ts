9b04e941505926c0c5bf5cd887040a93c57a1ec6
Hieu Do <mr.hieudt@gmail.com>
2017-07-15 17:29:15 -0500

    Install @angular/material

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +11,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
