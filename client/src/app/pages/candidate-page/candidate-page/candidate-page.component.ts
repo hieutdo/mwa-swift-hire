2c0547b1dc16571baf25658dc4c32b1d31cb6600
yuyang <summerkiss@me.com>
2017-07-17 10:45:17 -0500

    first commit of candidate page

---
+++ client/src/app/pages/candidate-page/candidate-page/candidate-page.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-candidate-page',
+  templateUrl: './candidate-page.component.html',
+  styleUrls: ['./candidate-page.component.scss']
+})
+export class CandidatePageComponent implements OnInit {
+
+  constructor() {
+    console.log("aaaaaaa");
+
+  }
+
+  ngOnInit() {
+  }
+
+}
