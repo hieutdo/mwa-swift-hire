1e7e3f0607d822bca7e213898146d6d2a7630085
yuyang <summerkiss@me.com>
2017-07-17 10:45:44 -0500

    first commit of candidate resume

---
+++ client/src/app/pages/candidate-page/candidate-resume/candidate-resume.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-candidate-resume',
+  templateUrl: './candidate-resume.component.html',
+  styleUrls: ['./candidate-resume.component.scss']
+})
+export class CandidateResumeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
