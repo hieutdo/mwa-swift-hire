08cdf75516e8371a95f7c7f5b1e2d58d6ff3f238
yuyang <summerkiss@me.com>
2017-07-18 15:46:03 -0500

    first commit of candidate resume

--- client/src/app/pages/candidate-page/candidate-jobs/candidate-jobs.component.ts
+++ client/src/app/pages/candidate-page/candidate-jobs/candidate-jobs.component.ts
@@ -1,13 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-candidate-jobs',
   templateUrl: './candidate-jobs.component.html',
-  styleUrls: ['./candidate-jobs.component.scss']
+  styleUrls: ['./candidate-jobs.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class CandidateJobsComponent implements OnInit {
+  autoTicks = false;
+  disabled = false;
+  invert = false;
+  max = 100;
+  min = 0;
+  showTicks = false;
+  step = 1;
+  thumbLabel = false;
+  value = 0;
+  vertical = false;
+
+
 
   constructor() { }
+
+  get tickInterval(): number | 'auto' {
+    return this.showTicks ? (this.autoTicks ? 'auto' : this._tickInterval) : 0;
+  }
+  set tickInterval(v) {
+    this._tickInterval = Number(v);
+  }
+  private _tickInterval = 1;
 
   ngOnInit() {
   }
--- client/src/app/pages/candidate-page/candidate-resume/candidate-resume.component.ts
+++ client/src/app/pages/candidate-page/candidate-resume/candidate-resume.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { StepState } from '@covalent/core';
 
 @Component({
   selector: 'app-candidate-resume',
@@ -6,8 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./candidate-resume.component.scss']
 })
 export class CandidateResumeComponent implements OnInit {
+  activeDeactiveStep1Msg: string = 'No select/deselect detected yet';
+  stateStep2: StepState = StepState.Required;
+  stateStep3: StepState = StepState.Complete;
+  disabled: boolean = false;
 
+  techs = ['Pizza', 'Pasta', 'Parmesan'];
   constructor() { }
+
+
+  toggleRequiredStep2(): void {
+    this.stateStep2 = (this.stateStep2 === StepState.Required ? StepState.None : StepState.Required);
+  }
+
+  toggleCompleteStep3(): void {
+    this.stateStep3 = (this.stateStep3 === StepState.Complete ? StepState.None : StepState.Complete);
+  }
+
+  activeStep1Event(): void {
+    this.activeDeactiveStep1Msg = 'Active event emitted.';
+  }
+
+  deactiveStep1Event(): void {
+    this.activeDeactiveStep1Msg = 'Deactive event emitted.';
+  }
+
 
   ngOnInit() {
   }
