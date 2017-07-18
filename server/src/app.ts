7a459fabc827bba037d726acbd662a63ecbbedc3
Hieu Do <mr.hieudt@gmail.com>
2017-07-18 10:10:01 -0500

    Add authentication checking to express app

--- server/src/app.ts
+++ server/src/app.ts
@@ -5,20 +5,28 @@ import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import * as morgan from 'morgan';
 import * as cors from 'cors';
-import { useContainer, useExpressServer } from 'routing-controllers';
+import * as jwt from 'express-jwt';
+import { Action, useContainer, useExpressServer } from 'routing-controllers';
 import { Container } from 'typedi';
 
 // load environment variables
 dotenv.config();
 
-const PORT = process.env.PORT || 4000;
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/swifthire';
+declare const process: {
+  env: {
+    PORT: number,
+    MONGODB_URI: string,
+    AUTH0_SECRET: string,
+    AUTH0_CLIENT_ID: string,
+  }
+  on: Function,
+  exit: Function
+};
 
-mongoose.connect(MONGODB_URI);
+const PORT = process.env.PORT;
+const MONGODB_URI = process.env.MONGODB_URI;
 
-mongoose.connection.on("connected", function() {
-  console.log("Connected to " + MONGODB_URI);
-
+function bootstrap() {
   // setup dependency injection container
   useContainer(Container);
 
@@ -32,26 +40,44 @@ mongoose.connection.on("connected", function() {
   app.use(cors());
   app.use(bodyParser.json());
 
+  const authCheck = jwt({
+    secret: new Buffer(process.env.AUTH0_SECRET, 'base64'),
+    audience: process.env.AUTH0_CLIENT_ID
+  });
+
   useExpressServer(app, {
     routePrefix: '/api',
-    controllers: [__dirname + '/controllers/**/*.js']
+    controllers: [__dirname + '/controllers/**/*.js'],
+    authorizationChecker: async (action: Action): Promise<boolean> => {
+      return new Promise<boolean>((resolve) => {
+        authCheck(action.request, action.response, (err) => {
+          return resolve(!err);
+        });
+      });
+    }
   });
 
   app.listen(PORT, () => {
     console.log('SwiftHire server listening on port ' + PORT);
   });
-});
+}
 
-mongoose.connection.on("error", function(error) {
-  console.log("Connection to " + MONGODB_URI + " failed:", error);
-});
+mongoose.connect(MONGODB_URI);
 
-mongoose.connection.on("disconnected", function() {
-  console.log("Disconnected from " + MONGODB_URI);
-});
+mongoose.connection
+  .on("connected", function () {
+    console.log("Connected to " + MONGODB_URI);
+    bootstrap();
+  })
+  .on("error", function (error) {
+    console.log("Connection to " + MONGODB_URI + " failed:", error);
+  })
+  .on("disconnected", function () {
+    console.log("Disconnected from " + MONGODB_URI);
+  });
 
-process.on("SIGINT", function() {
-  mongoose.connection.close(function() {
+process.on("SIGINT", function () {
+  mongoose.connection.close(function () {
     console.log("Disconnected from " + MONGODB_URI + " through app termination");
     process.exit(0);
   });
--- server/src/controllers/job.controller.ts
+++ server/src/controllers/job.controller.ts
@@ -1,7 +1,8 @@
-import { Get, JsonController } from 'routing-controllers';
+import { Authorized, Get, JsonController } from 'routing-controllers';
 import { Job } from '../models/job.model';
 
 @JsonController('/jobs')
+@Authorized()
 export class JobController {
 
   @Get('/')
